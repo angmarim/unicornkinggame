4588c4b0bed77281fd082de5c9898980847bab5e
Jean Paulo dos Santos Filho <jeanpsfilho@gmail.com>
2017-06-22 16:03:33 -0300

    Fix ~moveChar~ detecting keyboard buffer to compare
    Signed-off-by: Jean Paulo dos Santos Filho <jeanpsfilho@gmail.com>

--- main.asm
+++ main.asm
@@ -1,111 +1,120 @@
-model small
 org 100h         ;use for .com files, since it sets the current adress to 100h and .com files start ar h10 adress
-;jmp start
+INCLUDE emu8086.inc  
+
 .stack           ;reserve  quantity of memory for the stack, use for .exe since you need to create a stack of functions
 
 .data
     ;utils section
     scrCleaner DB '$'
+
     ;msgs section
     msg_horned db "You got horned.$"
     msg_dodge db "Do not get horned!$"
     ;objects section
-    char_main db "@$"
-    char_unicorn db "<$"
+    char_main db "@$" 
+    char_main_next db ?
+    char_unicorn db "<$" 
+    char_unicorn_next db ?
     ;variables section
     var_print_dodged_unicorns db "000$" ;hold string that represents the score
     var_dodged_unicorns dw 0            ;hold a number which means total score
-    
-    var_must_quit db 0                  ;flag when player quit
+ 
+    var_must_quit db 9                  ;flag when player quit
     
     var_pos_char_main dw 0              ;hold position of main character
     var_pos_char_unicorn dw 0           ;hold position of unicorn character
+ 
+    ;configuration section
+    startX db 39    ;start position of main char
+    startY db 24    ;start position of main char    
+             
+    move_right db 4
+    move_left db 6
     
-    X dw 0
-    Y dw 0
-    B dw 0
-    ;-----
     jmp start      
-
-    tam equ 0
-    cursor dw  tam dup(0) 
-
-    acima      equ     48h
-    abaixo    equ     50h  
-    parado    equ     52h
-
-    direcao db  parado 
-    prd1 db "************* $"
-    prd2 db "|$"
-
-    vertical equ $- prd1
-    horizontal equ $- prd2
-
-    msg     db "==== Como Jogar ====", 0dh,0ah
-    db "Use as setas p/ cima e p/ baixo para controlar o cursor", 0dh,0ah,0ah   
-    db "Aperte Esc para sair", 0dh,0ah
-    db "====================", 0dh,0ah, 0ah
-    db "Aperte qualquer tecla para comecar!$"
-    ;-----
 
     
 .code
 start:
     ;limpa buffer para print
     MOV ax, SEG scrCleaner
-    MOV ds,ax 
-    ;keyboard config
-    MOV ax, 0305h
-    XOR bx, bx
-    INT 16h
-    ;enter graphic mode, 300x200 8bit color
-    MOV ax, 13h 
-    INT 10h
+    MOV ds,ax  
 
-; print da mensagem
-mov dx, offset msg
-mov ah, 9
-int 21h   
+    ;Configuration section
+        ;set char position
+        MOV DH, startY      ;set y coordinate value for main char
+        MOV DL, startX      ;set x coord val to main char
+        GOTOXY DL, DH       ;place cursor at this position
+        ;print char in position
+        PUTC char_main      ;print main char in GOTOXY position
+        ;resets
+        XOR AL, AL          ;AL = 0 *see logic classes to understand*
+     
+     ;FUNCTIONS
+;MAIN LOGIC SECTION
+;###################################################################################
+        waitKeyPress:                                                             ;#
+            ;check if keystroke true/false                                        ;#
+            MOV AH, 1                                                             ;#
+            INT 16H         ;interruption to check if keyboard buffer have a value;#
+            ;if keystroke flag true goto                                          ;#
+            JNZ moveChar    ;if flag from interruption 1, go to move char function;#
+                returnFromMoveChar:                                               ;#
+            ;goto functions that move unicorns                                    ;#
+            JMP moveUnicorns                                                      ;#
+                returnMoveUnicorns:                                               ;#
+            ;keep checking all above forever other then....                       ;#
+            ;...missing if horned die                                             ;#
+            JMP waitKeyPress                                                      ;#
+;###################################################################################      
 
-; aguardando tecla para iniciar
-mov ah, 00h
-int 16h
+;LOGIC FOR MOVING CHAR OR UNICORNS OR BOTH SECTION
+;#############################################
+        moveChar:                           ;#
+            ;move keyboard buffer to ax     ;#
+            MOV AH, 1                       ;#
+            INT 16h                         ;#
+            CMP AH, 4                     ;#
+            JE  movLeft                     ;#
+            CMP AH, 6                     ;#
+            JE  movLeft                     ;#
+                returnAfterMove:            ;#
+            JMP returnFromMoveChar          ;#
+;#############################################     
 
-; escondendo o cursor de texto  
-mov ah, 1
-mov ch, 2bh
-mov cl, 0bh
-int 10h    
+;MOVE CHAR SECTION                         
+;#####################################################
+        movLeft:                                    ;#
+            ;set new position                       ;#
+            INC startX                              ;#
+            MOV DL, startX                          ;#
+            GOTOXY DL, DH                           ;#
+            call CLEAR_SCREEN                            ;#
+            ;print char again in new position       ;#
+            PUTC char_main                          ;#
+            XOR AL, AL          ;clean register     ;#
+            JMP returnAfterMove ;go back to loop    ;#
+                                                    ;#
+        movRight:                                   ;#
+            ;set new position                       ;#
+            INC startX                              ;#
+            MOV DL, startX                          ;#
+            GOTOXY DL, DH                           ;#
+            call CLEAR_SCREEN                            ;#
+            ;print char in new position             ;#
+            PUTC AL, AL                             ;#
+            XOR AL, AL                              ;#
+            JMP returnAfterMove ;go back to loop    ;#
+;#####################################################
 
-
-;parede1 prd1 
-;parede2 prd2
-
-loop_jogo:
-
-; desenhar campo
-
-
-; colocando cursor no inicio da tela
-mov al, 0
-mov ah, 05h
-int 10h
-
-mov dx, cursor
-
-mov ah, 02h 
-int 10h
-
-mov al, '@'
-mov ah, 09h
-mov bl, 0eh
-mov cx, 1
-int 10h
-
-
-
-    
+;MOVE UNICORNS SECTION  
+;#############################################
+        moveUnicorns:                       ;#
+            JMP returnMoveUnicorns          ;#
+;#############################################        
+DEFINE_CLEAR_SCREEN 
+DEFINE_SCAN_NUM
     ;stop process
     MOV AX, 4C00h ; prepare to interrupt process
-    INT 21h ; interrup process    
+    INT 21h ; interrup process
 end start
